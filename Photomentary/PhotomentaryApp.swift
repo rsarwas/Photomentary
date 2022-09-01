2db8164932aca1356f06eb9e78f78655feee05fe
Regan Sarwas <rsarwas@gmail.com>
2022-08-31 19:43:38 -0800

    Initial Commit

---
+++ Photomentary/PhotomentaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotomentaryApp.swift
+//  Photomentary
+//
+//  Created by Regan Sarwas on 8/31/22.
+//
+
+import SwiftUI
+
+@main
+struct PhotomentaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
